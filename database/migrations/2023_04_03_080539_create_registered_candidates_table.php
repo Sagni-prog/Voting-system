dbafa747b56d7c7fe3363b88d34407209baf3800
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-04-03 11:12:27 +0300

    ...

---
+++ database/migrations/2023_04_03_080539_create_registered_candidates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registered_candidates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vote_id')->unsigned();
+            $table->integer('candidate_id')->unsigned();
+            $table->boolean('session_status')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registered_candidates');
+    }
+};
