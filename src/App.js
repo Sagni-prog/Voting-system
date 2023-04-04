2447b52cf183c674900a7b2a9591e9929e40698d
Natnael Getachew <natnaeln4d@gmail.com>
2023-04-04 19:37:51 +0300

    home some changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
           <div className='w-[100%] '>
     {/* <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg> */}
-
+  
     <nav className=" h-[80px] border-gray-200 dark:bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="#" className="flex items-center">
@@ -57,7 +57,7 @@ function App() {
 
         </div>
         <div className="flex-1 w-50">
-          <div className="absolute ml-[3rem] mt-[-3rem]">
+          <div className="absolute ml-[3rem] mt-[-1.5rem]">
           <img class="image w-70 absolute rounded-[100%]" src={img} alt="user photo"/>
           <img class="image1  w-[70vh] h-[70vh]  rounded-[150%]" src={image} alt="user photo"/>
           </div>
