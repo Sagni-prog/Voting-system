a03409d188745de392b820fab2654d7cfb6b9f0e
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-05-13 20:49:54 +0300

    merge

--- src/http/http.js
+++ src/http/http.js
@@ -1,14 +1,12 @@
 import axios from "axios"
     
      const token = localStorage.getItem('token');
-     const faceId = localStorage.getItem('face-id');
      const http = axios.create({
         'baseURL': 'http://localhost:8000/api',
         
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`,
-          'X-Face-Id' : faceId ? faceId : 'hjhknj',
         }
      });
      
