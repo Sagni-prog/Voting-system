cb07262b09745f8e7513b0bccfc4bc27dfbc2c47
Natnael Getachew <natnaeln4d@gmail.com>
2023-05-14 15:32:22 +0300

    some change

--- src/componets/Nav/Navbar.js
+++ src/componets/Nav/Navbar.js
@@ -10,6 +10,9 @@ export default function Navbar() {
     const [visisble,setVisible]=useState(false)
     function clicked(){
         setIsChecked(isChecked)
+          setTimeout(() => {
+            setIsChecked(false);
+    }, 4000)
     }
 
   return (
@@ -132,7 +135,7 @@ export default function Navbar() {
                             <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <a  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={setIsChecked(false)} >cancel</a>
                   </div>
                   <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
